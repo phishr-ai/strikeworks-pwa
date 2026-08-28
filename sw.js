bbc5fd29c88083dad37f7f1c6f1f511654cdb6eb
Omar Gerardo <62450194+inix-x@users.noreply.github.com>
2026-08-29 03:49:44 +0800

    Bump cache to v14 for the commission payout change
    Without this the service worker serves the old index.html and the numbers look
    frozen, which is what happened on 2026-04-30.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'sw-commissions-v13';
+var CACHE_NAME = 'sw-commissions-v14';
 var APP_SHELL = ['/', '/index.html', '/manifest.json', '/favicon.ico', '/icons/logo.png', '/icons/icon-192x192.png', '/icons/icon-512x512.png', '/icons/apple-touch-icon.png'];
 
 self.addEventListener('install', function (e) {
